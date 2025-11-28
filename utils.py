78f4405df72e95cd3efff5d6109d6cbee1f456ba
d41dev <d41dev@proton.me>
2025-11-28 10:51:46 +0000

    update: add utils for dyanmic file naming

--- utils.py
+++ utils.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 import os
 
 def read_dataset_dir(dataset_dir):
@@ -23,3 +21,31 @@ def load_class_names(class_names_path):
     with open(class_names_path, "r") as file:
         class_names = [line.strip() for line in file.readlines()]
     return class_names
+
+
+def get_new_dirname(dir_path: str, prefix: str):
+    os.makedirs(dir_path, exist_ok=True)
+    highest = -1
+    dirs = os.listdir(dir_path)
+    if len(dirs) == 0:
+        return os.path.join(dir_path, prefix + "0")
+    for d in dirs:
+        if not d.startswith(prefix):
+            continue
+        n = int(d.strip(prefix))
+        highest = n if n > highest else highest
+    return os.path.join(dir_path, prefix + str(highest + 1))
+
+
+def get_new_filename(dir_path: str, prefix: str, ext):
+    os.makedirs(dir_path, exist_ok=True)
+    highest = -1
+    files = os.listdir(dir_path)
+    if len(files) == 0:
+        return os.path.join(dir_path,  prefix + "0" + ext)
+    for f in files:
+        if not f.startswith(prefix):
+            continue
+        n = int(os.path.splitext(f)[0].strip(prefix))
+        highest = n if n > highest else highest
+    return os.path.join(dir_path, prefix + str(highest + 1) + ext)
