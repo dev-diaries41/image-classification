364101f1daa57dfe2234ca094fdcfad678d590e6
d41dev <d41dev@proton.me>
2025-11-28 21:38:22 +0000

    update: return classnames in read_dataset_dir

--- utils.py
+++ utils.py
@@ -6,21 +6,16 @@ def read_dataset_dir(dataset_dir):
 
     file_paths = []
     labels = []
-    # Iterate over all items in the dataset directory
-    for class_name in os.listdir(dataset_dir):
-        class_dir = os.path.join(dataset_dir, class_name)  # Construct the full path
+    class_names = os.listdir(dataset_dir)
+
+    for class_name in class_names:
+        class_dir = os.path.join(dataset_dir, class_name) 
         if os.path.isdir(class_dir):
             class_dir_files = os.listdir(class_dir)
-            for path in class_dir_files:
-                file_paths.append(os.path.join(class_dir, path))
+            for filename in class_dir_files:
+                file_paths.append(os.path.join(class_dir, filename))
                 labels.append(class_name)
-    return file_paths, labels
-
-
-def load_class_names(class_names_path):
-    with open(class_names_path, "r") as file:
-        class_names = [line.strip() for line in file.readlines()]
-    return class_names
+    return file_paths, labels, class_names
 
 
 def get_new_dirname(dir_path: str, prefix: str):
