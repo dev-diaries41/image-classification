4186614943d435d13779d83d999d0ea403cf0a5a
d41dev <d41dev@proton.me>
2025-11-28 16:55:22 +0000

    update: simplify evaluate function

--- train.py
+++ train.py
@@ -6,30 +6,6 @@ from torch.utils.data import DataLoader, random_split
 from utils import read_dataset_dir
 from data import ImageDataset
 
-def evaluate(model, data_loader, device, criterion):
-    model.eval()
-    total_loss = 0.0
-    correct = 0
-    total = 0
-    use_hebb = hasattr(model, "mlp")
-
-    with torch.no_grad():
-        for batch in data_loader:
-            images = batch["image"].to(device)
-            labels = batch["label"].to(device)
-            if use_hebb:
-                y_pred, _ = model(images, return_activations = False)
-            else:
-                 y_pred = model(images)
-            loss = criterion(y_pred, labels)
-            total_loss += loss.item()
-            preds = y_pred.argmax(dim=1)
-            correct += (preds == labels).sum().item()
-            total += labels.size(0)
-    avg_loss = total_loss / len(data_loader)
-    accuracy = correct / total if total > 0 else 0
-    model.train()  # Switch back to training mode
-    return avg_loss, accuracy
 
 def train(model, dataset_dir, class_names, device, checkpoint_path, final_model_path, epochs=100, lr=0.001, patience=10, lr_patience=3, factor=0.5, min_lr=1e-6, batch_size = 8):
     model.to(device)
@@ -55,7 +31,6 @@ def train(model, dataset_dir, class_names, device, checkpoint_path, final_model_
     train_dataset, test_dataset = random_split(dataset, [train_size, test_size])
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
     test_loader = DataLoader(test_dataset, batch_size=1, shuffle=False)
-
     
     for epoch in range(epochs):
         running_loss = 0.0
@@ -67,7 +42,7 @@ def train(model, dataset_dir, class_names, device, checkpoint_path, final_model_
             if use_hebb:
                 y_pred, acts = model(images, return_activations = True)
             else:
-                 y_pred = model(images)
+                 y_pred, _ = model(images)
             loss = criterion(y_pred, labels)
             loss.backward()
             
@@ -83,7 +58,7 @@ def train(model, dataset_dir, class_names, device, checkpoint_path, final_model_
 
         # Evaluate on test set if provided
         if test_loader:
-            avg_test_loss, test_accuracy = evaluate(model, test_loader, device, criterion)
+            avg_test_loss, test_accuracy = evaluate(model, test_loader)
             test_history.append(avg_test_loss)
             test_acc_history.append(test_accuracy)
             print(f"Epoch {epoch+1} Test Loss: {avg_test_loss:.4f}, Test Accuracy: {test_accuracy:.4f}")
@@ -109,3 +84,32 @@ def train(model, dataset_dir, class_names, device, checkpoint_path, final_model_
     torch.save(model.state_dict(), final_model_path)
     print(f"Final model saved: {final_model_path}")
     return train_history, test_history, test_acc_history
+
+
+
+def evaluate(model, data_loader):
+    model.eval()
+    total_loss = 0.0
+    correct = 0
+    total = 0
+    use_hebb = hasattr(model, "mlp")
+    criterion = torch.nn.CrossEntropyLoss()
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+    with torch.no_grad():
+        for batch in data_loader:
+            images = batch["image"].to(device)
+            labels = batch["label"].to(device)
+            if use_hebb:
+                y_pred, _ = model(images, return_activations = False)
+            else:
+                 y_pred = model(images)
+            loss = criterion(y_pred, labels)
+            total_loss += loss.item()
+            preds = y_pred.argmax(dim=1)
+            correct += (preds == labels).sum().item()
+            total += labels.size(0)
+    avg_loss = total_loss / len(data_loader)
+    accuracy = correct / total if total > 0 else 0
+    model.train()  # Switch back to training mode
+    return avg_loss, accuracy
