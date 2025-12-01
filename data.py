f5489bc0a4161fbad1cfc0e7c102aced79198ad3
d41dev <d41dev@proton.me>
2025-12-01 10:38:28 +0000

    update: add flipping to transformation steps to make model more robust

--- data.py
+++ data.py
@@ -18,6 +18,7 @@ class ImageDataset(Dataset):
         if transform is None:
             self.transform = transforms.Compose([
                 transforms.Resize((224, 224)),
+                transforms.RandomHorizontalFlip(),
                 transforms.ToTensor(),
                 transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                      std=[0.229, 0.224, 0.225])
